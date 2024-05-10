d947944f1d8f9066a28268525aa98c77a0d78031
Ayush351 <payush351@gmail.com>
2024-05-10 08:01:51 +0530

    Sort 0 1 2

--- Striver/Array/Sort012.cpp
+++ Striver/Array/Sort012.cpp
@@ -1,12 +1,39 @@
-
+/*
+Problem Statement: Given an array consisting of only 0s, 1s, and 2s. Write a program to in-place sort the array without using inbuilt sort functions. ( Expected: Single pass-O(N) and constant space)
+*/
 #include<bits/stdc++.h>
 using namespace std;
 //#define int long long
 int T, N;
 #define p(i,n,vect) for(int i=0;i<(int)n;++i) {cout<<vect[i]<<" ";} cout<<"\n";
+void sortZeroOneTwo(int *arr, int N)
+{
+    int low = 0, mid = 0, high = N - 1;
+    while (mid <= high)
+    {
+        switch (arr[mid])
+        {
+        case 0:
+            swap(arr[low++], arr[mid++]);
+            break;
+        case 1:
+            mid++;
+            break;
+        case 2:
+            swap(arr[mid], arr[high--]);
+            break;
+        }
+    }
+}
 void solve()
 {
     cin>>N;
+    int *arr=new int[N];
+    for (int i = 0; i <(int) N; i++)
+    {
+        cin>>arr[i];
+    }
+    sortZeroOneTwo(arr,N);
 }
 signed main()
 {
